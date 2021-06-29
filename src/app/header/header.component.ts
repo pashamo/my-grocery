661aa59d8464db16c2a07f29020230f147986771
Mohammed Pasha <mk.pasha@hotmail.com>
2021-06-29 14:06:29 -0400

    feat: Created shell components

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    template: '<p>app-header</p>',
+    styles: ['']
+})
+
+export class HeaderComponent {
+    
+}
