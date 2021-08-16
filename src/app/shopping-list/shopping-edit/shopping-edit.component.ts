952e8aa05c021cfe300ffe40597745b28749b3c4
Mohammed Pasha <mk.pasha@hotmail.com>
2021-08-16 16:09:05 -0400

    feat: Shopping list add button functionailty

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() newIngredient = new EventEmitter<Ingredient>();
+  @ViewChild('nameInput') nameInputRef: ElementRef | undefined;
+  @ViewChild('amountInput') amountInputRef: ElementRef | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAdd() {
+    this.newIngredient.emit(new Ingredient(this.nameInputRef?.nativeElement.value, this.amountInputRef?.nativeElement.value));
+  }
 }
