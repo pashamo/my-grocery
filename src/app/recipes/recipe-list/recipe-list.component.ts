35f6477a27e110b2a2f1217ec778a789821b4087
Mohammed Pasha <mk.pasha@hotmail.com>
2021-08-16 15:42:32 -0400

    feat: data flow from recipe item to recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'Southern Pimento Cheese',
@@ -30,4 +31,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSelectedRecipe(data: Recipe) {
+    this.selectedRecipe.emit(data);
+  }
 }
