816717b432e62a932e148d1afcb72badc64d8a1f
Mohammed Pasha <mk.pasha@hotmail.com>
2021-08-16 13:47:34 -0400

    feat: Added navigation to recipe and shopping list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-grocery';
+  selected: string = "RECIPE";
+
+  onNavigate = (data: string) => this.selected = data;
 }
